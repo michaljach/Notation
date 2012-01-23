d237d1f7764695a0d95285203699646f9321de8b
Michal Jach <ja@michaljach.pl>
2012-01-23 12:49:47 +0100

    Initial Commit

---
+++ NotationTests/NotationTests.h
@@ -0,0 +1,13 @@
+//
+//  NotationTests.h
+//  NotationTests
+//
+//  Created by Michał Jach on 23.01.2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NotationTests : SenTestCase
+
+@end
---
+++ NotationTests/NotationTests.m
@@ -0,0 +1,32 @@
+//
+//  NotationTests.m
+//  NotationTests
+//
+//  Created by Michał Jach on 23.01.2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "NotationTests.h"
+
+@implementation NotationTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NotationTests");
+}
+
+@end
